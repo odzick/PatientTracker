274aa009df0fd9199202bccb76db8d7fabd4a074
odzick <odzick@gmail.com>
2017-03-30 17:09:56 -0700

    add preview activity for sending image

---
+++ app/src/main/java/group12/cpen391/patienttracker/SendImageActivity.java
@@ -0,0 +1,71 @@
+package group12.cpen391.patienttracker;
+
+import android.graphics.Bitmap;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+
+import java.util.Arrays;
+
+public class SendImageActivity extends AppCompatActivity implements AdapterView.OnClickListener{
+
+    ImageView mToSendView;
+    FloatingActionButton mConfirmFab;
+    Bitmap toSendBitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_image);
+
+        mToSendView = (ImageView) findViewById(R.id.to_send_image_view);
+        mConfirmFab = (FloatingActionButton) findViewById(R.id.confirm_image_fab);
+
+        toSendBitmap = (Bitmap) getIntent().getParcelableExtra("IMG_BITMAP");
+
+        mToSendView.setImageBitmap(toSendBitmap);
+        mConfirmFab.setOnClickListener( this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case(R.id.confirm_image_fab):
+                sendBitmap(toSendBitmap);
+                break;
+        }
+    }
+
+    void sendBitmap(Bitmap imageBitmap){
+        int pixels[] = new int[250  * 250];
+        byte bytePixels[] = new byte[250 * 250];
+
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(imageBitmap, 250, 250, false);
+        scaledBitmap.getPixels(pixels, 0 , 250, 0, 0, 250, 250);
+        Log.i ("IMAGE", String.format("pixel 0,0 " + String.format( "0x%08X", imageBitmap.getPixel(0, 0))));
+        Log.i("IMAGE", "pixel buffer " + Arrays.toString(pixels));
+        Log.i("IMAGE", "image size " + pixels.length);
+
+        ARGBto6bitRGB(pixels, bytePixels);
+        String toSend = new String(bytePixels);
+        Log.i("IMAGE", "pixel buffer " + toSend);
+
+        BluetoothService bt = BluetoothService.getService();
+        bt.write("~" + toSend);
+    }
+
+    void ARGBto6bitRGB(int[] ARGB, byte[] RGB){
+        int pixelIn;
+        int pixel;
+
+        for (int i = 0; i <  ARGB.length; i++){
+            pixelIn = ARGB[i];
+            pixel = ((pixelIn & 0xc00000) >> 18) | ((pixelIn & 0xc000) >> 12) | ((pixelIn & 0xc0) >> 6);
+            RGB[i] = (byte) pixel;
+        }
+    }
+}
--- app/src/main/java/group12/cpen391/patienttracker/SettingsFragment.java
+++ app/src/main/java/group12/cpen391/patienttracker/SettingsFragment.java
@@ -222,7 +222,10 @@ public class SettingsFragment extends Fragment implements AdapterView.OnClickLis
             if(requestCode == REQUEST_IMAGE_CROP){
                 Bundle extras = data.getExtras();
                 Bitmap imageBitmap = (Bitmap) extras.get("data");
-                sendBitmap(imageBitmap);
+                Intent intent = new Intent(this.getContext(), SendImageActivity.class);
+                intent.putExtra("IMG_BITMAP", imageBitmap);
+                startActivity(intent);
+                //sendBitmap(imageBitmap);
             }
         }
     }
@@ -244,36 +247,6 @@ public class SettingsFragment extends Fragment implements AdapterView.OnClickLis
             String errorMessage = "Whoops - your device doesn't support the crop action!";
             Toast toast = Toast.makeText(this.getContext(), errorMessage, Toast.LENGTH_SHORT);
             toast.show();
-        }
-    }
-
-
-    void sendBitmap(Bitmap imageBitmap){
-        int pixels[] = new int[250  * 250];
-        byte bytePixels[] = new byte[250 * 250];
-
-        Bitmap scaledBitmap = Bitmap.createScaledBitmap(imageBitmap, 250, 250, false);
-        scaledBitmap.getPixels(pixels, 0 , 250, 0, 0, 250, 250);
-        Log.i ("IMAGE", String.format("pixel 0,0 " + String.format( "0x%08X", imageBitmap.getPixel(0, 0))));
-        Log.i("IMAGE", "pixel buffer " + Arrays.toString(pixels));
-        Log.i("IMAGE", "image size " + pixels.length);
-
-        ARGBto6bitRGB(pixels, bytePixels);
-        String toSend = new String(bytePixels);
-        Log.i("IMAGE", "pixel buffer " + toSend);
-
-        BluetoothService bt = BluetoothService.getService();
-        bt.write("~" + toSend);
-    }
-
-    void ARGBto6bitRGB(int[] ARGB, byte[] RGB){
-        int pixelIn;
-        int pixel;
-
-        for (int i = 0; i <  ARGB.length; i++){
-            pixelIn = ARGB[i];
-            pixel = ((pixelIn & 0xc00000) >> 18) | ((pixelIn & 0xc000) >> 12) | ((pixelIn & 0xc0) >> 6);
-            RGB[i] = (byte) pixel;
         }
     }
 }
