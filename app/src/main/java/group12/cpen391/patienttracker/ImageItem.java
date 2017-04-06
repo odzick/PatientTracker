2baac7104c8adf75e4183d6be7e45599beecef48
Derek Tam <derekkx@gmail.com>
2017-04-05 18:02:15 -0700

    Formatted image dates

--- app/src/main/java/group12/cpen391/patienttracker/ImageItem.java
+++ app/src/main/java/group12/cpen391/patienttracker/ImageItem.java
@@ -1,6 +1,11 @@
 package group12.cpen391.patienttracker;
 
 import android.graphics.Bitmap;
+import android.util.Log;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ImageItem {
     public int id;
@@ -9,7 +14,17 @@ public class ImageItem {
 
     public ImageItem(int id, String date, Bitmap image){
         this.id = id;
-        this.date = date;
+        this.date = formatDate(date);
         this.image = image;
+    }
+
+    private String formatDate(String date){
+        try {
+            Date d = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(date);
+            return new SimpleDateFormat("MMMM d, yyyy h:mm a").format(d);
+        } catch(ParseException e){
+            Log.e("ImageItem", e.toString());
+            return date;
+        }
     }
 }
